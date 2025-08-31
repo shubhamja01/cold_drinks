3da29ee998449eec8c5b32e77b67b0e8292bbef1
Šhübhäm Jάωäιξ <xymbabpq@outlook.com>
2025-08-31 19:14:01 +0530

    data

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cold_drinks/';
+$config['base_url'] = 'https://shubhamja01.github.io/cold_drinks/';
 
 /*
 |--------------------------------------------------------------------------
